ea0221fdc9c6c417ac4cffe083187a361cde7eb9
Anson <ansonchan.dudu@gmail.com>
2017-04-25 12:12:37 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+function $(id) {
+	return document.getElementById(id);
+}
+
+function hasClass(obj, cls) {
+    return obj.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+}
+
+function removeClass(obj, cls) {
+    if (hasClass(obj, cls)) {
+        var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+        obj.className = obj.className.replace(reg, ' ');
+    }
+}
+
+$('button3').addEventListener('click', function(){
+	removeClass($('mute'),'hidden');
+	removeClass($('button2'),'animated');
+	removeClass($('button3'),'animated');
+});
